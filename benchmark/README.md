919aa801dbcf7e1acedbd811809a9c82f54989cd
Dylan Frankland <dylan@frankland.io>
2018-01-29 10:33:59 -0800

    Update Benchmarks

---
+++ benchmark/README.md
@@ -0,0 +1,16 @@
+# eyeliner-benchmark
+
+`eyeliner` is ~300% faster than [`juice`][juice].
+
+## Results
+
+```
+Juice x 2,676 ops/sec ±3.46% (75 runs sampled) - small HTML + CSS
+Eyeliner x 13,996 ops/sec ±1.32% (83 runs sampled) - small HTML + CSS
+Fastest is Eyeliner - small HTML + CSS
+Juice x 4.57 ops/sec ±5.05% (16 runs sampled) - large HTML + CSS
+Eyeliner x 13.80 ops/sec ±1.56% (37 runs sampled) - large HTML + CSS
+Fastest is Eyeliner - large HTML + CSS
+```
+
+[juice]: https://github.com/Automattic/juice
--- benchmark/src/index.js
+++ benchmark/src/index.js
@@ -3,43 +3,76 @@
 import { Suite } from 'benchmark';
 import juice from 'juice';
 import eyeliner from 'eyeliner';
+import { readFileSync } from 'fs';
+import { resolve as resolvePath } from 'path';
 
-const fixture = `
-  <!doctype html>
-  <html>
-    <head>
-      <style>
-        .red {
-          color: red;
-        }
-      </style>
-    </head>
-    <body>
-      <h1 class="red">This is red</h1>
-    </body>
-  </html>
-`;
+const createSuite = (title) => {
+  const suite = new Suite();
 
-console.log(`## Test HTML: ${fixture}`);
-console.log(`### Juice:\n${juice(fixture)}`);
-console.log(`### Eyeliner:\n${eyeliner({ html: fixture })}`);
+  suite.on('cycle', (event) => {
+    console.log(`${event.target} - ${title}`);
+  });
 
-const suite = new Suite();
+  suite.on('complete', () => {
+    console.log(`Fastest is ${suite.filter('fastest').map('name')} - ${title}`);
+  });
 
-suite.add('Juice', () => {
-  juice(fixture);
-});
+  return {
+    add: (...args) => suite.add(...args),
+    run: () => new Promise((resolve) => {
+      suite.run({ async: true });
+      suite.on('complete', resolve);
+    }),
+  };
+};
 
-suite.add('Eyeliner', () => {
-  eyeliner({ html: fixture });
-});
+(async () => {
+  // Small HTML + CSS
+  const smallHtmlCss = `
+    <!doctype html>
+    <html>
+      <head>
+        <style>
+          .red {
+            color: red;
+          }
+        </style>
+      </head>
+      <body>
+        <h1 class="red">This is red</h1>
+      </body>
+    </html>
+  `;
 
-suite.on('cycle', (event) => {
-  console.log(`${event.target}`);
-});
+  console.log(`## Test HTML: ${smallHtmlCss}`);
+  console.log(`### Juice:\n${juice(smallHtmlCss)}`);
+  console.log(`### Eyeliner:\n${eyeliner({ html: smallHtmlCss })}`);
 
-suite.on('complete', () => {
-  console.log(`Fastest is ${suite.filter('fastest').map('name')}`);
-});
+  const smallSuite = createSuite('small HTML + CSS');
 
-suite.run({ async: true });
+  smallSuite.add('Juice', () => {
+    juice(smallHtmlCss);
+  });
+
+  smallSuite.add('Eyeliner', () => {
+    eyeliner({ html: smallHtmlCss });
+  });
+
+  await smallSuite.run();
+
+  // Large HTML + CSS
+  const largeHtml = readFileSync(resolvePath(__dirname, '../src/bootstrap/docs/4.0/examples/navbars/index.html'), { encoding: 'utf8' });
+  const largeCss = readFileSync(resolvePath(__dirname, '../src/bootstrap/dist/css/bootstrap.min.css'), { encoding: 'utf8' });
+
+  const largeSuite = createSuite('large HTML + CSS');
+
+  largeSuite.add('Juice', () => {
+    juice.inlineContent(largeHtml, largeCss);
+  });
+
+  largeSuite.add('Eyeliner', () => {
+    eyeliner({ html: largeHtml, css: largeCss });
+  });
+
+  await largeSuite.run();
+})();
